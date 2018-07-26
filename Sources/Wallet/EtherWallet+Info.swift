ac270e5be87655d8163c3dbe2a28f287d186d883
Steve Chang <steadyaction@gmail.com>
2018-07-26 20:57:16 +0900

    change return type of decimalsForToken to Int

--- Sources/Wallet/EtherWallet+Info.swift
+++ Sources/Wallet/EtherWallet+Info.swift
@@ -1,23 +1,29 @@
 import web3swift
 
 public protocol InfoService {
-    func decimalsForTokenSync(address: String) throws -> String
-    func decimalsForToken(address: String, completion: @escaping (String?) -> ())
+    func decimalsForTokenSync(address: String) throws -> Int
+    func decimalsForToken(address: String, completion: @escaping (Int?) -> ())
     func symbolForTokenSync(address: String) throws -> String
     func symbolForToken(address: String, completion: @escaping (String?) -> ())
 }
 
 extension EtherWallet: InfoService {
-    public func decimalsForTokenSync(address: String) throws -> String {
+    public func decimalsForTokenSync(address: String) throws -> Int {
         let contract = try erc20contract(address: address)
         let parameters = [AnyObject]()
         let decimalCallResult = contract.method("decimals", parameters: parameters, extraData: Data(), options: options)?.call(options: nil)
-        guard case .success(let decimalsInfo)? = decimalCallResult, let decimals = decimalsInfo["0"] else { throw WalletError.networkFailure }
+        guard case .success(let decimalsInfo)? = decimalCallResult, let decimals = decimalsInfo["0"] else { throw
+            WalletError.networkFailure
+        }
         
-        return String(describing: decimals)
+        guard let result = decimals as? Int else {
+            throw WalletError.unexpectedResult
+        }
+        
+        return result
     }
     
-    public func decimalsForToken(address: String, completion: @escaping (String?) -> ()) {
+    public func decimalsForToken(address: String, completion: @escaping (Int?) -> ()) {
         DispatchQueue.global().async {
             let decimals = try? self.decimalsForTokenSync(address: address)
             DispatchQueue.main.async {
