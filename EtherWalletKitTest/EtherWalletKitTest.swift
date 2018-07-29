c403c980ab5516262f57c8c207aba05be726b86b
Steve Chang <steadyaction@gmail.com>
2018-07-29 20:23:19 +0900

    add balance test cases

--- EtherWalletKitTest/EtherWalletKitTest.swift
+++ EtherWalletKitTest/EtherWalletKitTest.swift
@@ -34,4 +34,9 @@ class EtherWalletKitTest: XCTestCase {
         XCTAssert(EtherWallet.account.verifyPassword(password))
         XCTAssertFalse(EtherWallet.account.verifyPassword("WRONG_PASSWORD"))
     }
+    
+    func testBalance() {
+        XCTAssert(try! EtherWallet.balance.etherBalanceSync() == "0")
+        XCTAssert(try! EtherWallet.balance.tokenBalanceSync(contractAddress: "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07") == "0")
+    }
 }
